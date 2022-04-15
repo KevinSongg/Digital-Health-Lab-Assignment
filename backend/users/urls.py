0b8da248a137816429dbb6f8bf956303ef2076bd
Kunlun (Kevin) Song <48466527+KevinSongg@users.noreply.github.com>
2022-04-14 17:51:09 -0700

    Add files via upload

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # /users/add/
+    path('add/', views.add, name='add'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,17 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from .models import User
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
     users = serializers.serialize("json", User.objects.all())
     return HttpResponse(users)
+@csrf_exempt
+def add(request):
+    body = json.loads(request.body.decode('utf-8'))
+    user = User(body["name"], body["email"], body["phone"])
+    user.save()
+    return HttpResponse("Add user response." )
