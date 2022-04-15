549444a17437c370bdb36c74d18e37e87bf50144
Kunlun (Kevin) Song <48466527+KevinSongg@users.noreply.github.com>
2022-04-14 17:49:59 -0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ const url = "http://localhost:8000";
 function App() {
   const [users, setUsers] = useState();
   const [isFetching, setFetching] = useState(false);
+  const [newUserCount, setNewUserCount] = useState(0);
 
   const fetchUsers = async () => {
     setFetching(true);
@@ -30,8 +31,30 @@ function App() {
 
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [newUserCount]);
 
+  
+  const addUser = async () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const postData = {name, email, phone};
+    console.log(postData);
+
+    fetch(url + "/users/add/", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'}, 
+      body: JSON.stringify(postData)
+    }).then(res => {
+      console.log("Post response:", res);
+    });
+    setNewUserCount(newUserCount+1); 
+
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("phone").value="";
+
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -45,6 +68,16 @@ function App() {
           </ul>
         )}
       </header>
+      <div>
+         Name:<input id="name"></input> 
+      </div>
+      <div>
+        Email: <input id="email"></input> 
+      </div>
+      <div>
+        Phone:<input id="phone"></input> 
+      </div>
+      <button className="Add-button" onClick={addUser}>Add User</button>
     </div>
   );
 }
